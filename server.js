5c45465668f4db5d060fe15813d1d662b0f1dd50
Glitch (quintessential-crest) <none>
2019-12-25 09:18:38 +0000

    😜☄️ Checkpoint ./server.js:9636892/263

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-
 const cors = require('cors')
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.use(cors())
-
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use(express.static('public'));
 
-
-app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post("/api/exercise/new-user",(req, res)=>{
+  
+});
 
 // Not found middleware
 app.use((req, res, next) => {
